01d6f391dc90be679d670f900c0f38338b6c757d
googlockgood@gmail.com <googlockgood@gmail.com>
2017-03-06 03:22:32 +0300

    beta without DB

--- app/urls.py
+++ app/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
 
     url(r'^listdata/$', views.listdata, name='listdata'),
     # url(r'^delete_data/(?P<pk>[0-9]+)/$', views.delete_data, name='delete_data'),
-    url(r'^deletedata/(?P<pk>[0-9]+)/$', views.delete_data, name='delete_data'),
+    # url(r'^deletedata/(?P<pk>[0-9]+)/$', views.delete_data, name='delete_data'),
 
     # url(r'^delete_edit/(?P<pk>[0-9]+)/edit/$', views.delete_edit, name='delete_edit'),
     url(r'^data_edit/(?P<pk>[0-9]+)/edit/$', views.data_edit, name='data_edit'),
+    # url(r'^delete/(?P<pk>\d+)/$', views.delete, name='delete'),
+    # url(r'^delete/(?P<pk>\d+)/$', views.delete, name='delete'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
+from django.views.generic import DeleteView
+
+from django.http import HttpResponseNotFound, Http404
+
 from django.shortcuts import render, redirect, HttpResponseRedirect, get_object_or_404
 from django.http import HttpResponse
 from app.models import Word
 import random
 from .forms import WordForm
 from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
 
 def index(request):
     db = list(Word.objects.all())
@@ -30,10 +35,14 @@ def index(request):
     })
 
 def addition(request):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
+
     amount = Word.objects.all()
     if request.method == "POST":
         form = WordForm(request.POST)
-        # print(form.as_p())
+        print(form.as_p())
         # en = form['english']
         if form.is_valid():
             form.save()
@@ -44,26 +53,42 @@ def addition(request):
     'amount': len(amount)})
 
 def search_form(request):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
     return render_to_response('app/search_form.html')
 
 def data_edit(request, pk):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
     amount = Word.objects.all()
     some = get_object_or_404(Word, pk=pk)
-    if request.method == "POST":
+
+    if request.POST.get('delete'):
+        some.delete()
+        return HttpResponseRedirect("/listdata")
+    
+    # if request.method == "POST":
+    if request.POST.get('save'):
         form = WordForm(request.POST, instance=some)
         if form.is_valid():
             form.save()
             return redirect('/listdata', pk=some.pk)
     else:
         form = WordForm(instance=some)        
-    return render(request, 'app/addition.html', {'form': form, 'amount': len(amount)})
+    return render(request, 'app/data_edit.html', {'form': form, 'amount': len(amount)})
             
 
 def search(request):
-    db = Word.objects.all()
-    list_db = []
-    for ru in db:
-        list_db.append(ru.russia)
+
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
+    # db = Word.objects.all()
+    # list_db = []
+    # for ru in db:
+        # list_db.append(ru.russia)
     errors = []
     if 'q' in request.GET:
         q = request.GET['q']
@@ -72,8 +97,8 @@ def search(request):
             errors.append('Enter a search term')
         elif len(q) > 15:
             errors.append('Please you entered too much characters')
-        elif 'q' not in list_db:
-            errors.append("Not found") 
+        # elif 'q' not in list_db:
+            # errors.append("Not found") 
         else:
             data_ = Word.objects.filter(russia__icontains=q)
             return render_to_response('app/result.html',
@@ -81,6 +106,9 @@ def search(request):
     return render_to_response('app/rootroom.html', {'errors': errors})
 
 def demo(request):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
     number = 0
     if request.method == "POST":
         count = request.POST['something']
@@ -91,12 +119,14 @@ def demo(request):
         return render(request, 'app/demo.html', {'number': number})
 
 def rootroom(request):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
+
     return render(request, 'app/rootroom.html')
 
 def listdata(request):
+    if not request.user.is_authenticated():
+        return HttpResponseNotFound('<h1>Page not found</h1>')
     data = Word.objects.all()
     return render(request, 'app/listdata.html', {'data': data})
 
-def delete_data(request, pk):
-    some = get_object_or_404(Word, pk=pk)
-    return render(request, 'app/delete_data.html', {"some": some})
